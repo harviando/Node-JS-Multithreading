4a42d0629a12f0b2882232ba1e2e2b5694fa62f7
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-11-11 11:41:42 +0700

    splitting the CPU intensive task then run in concurrently between set number of workers (multithread)

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const jobs = Array.from({length: 100}, () => 1e9)
+const { Worker } = require("worker_threads");
 
-const tick = performance.now();
+const jobs = Array.from({ length: 100 }, () => 1e9)
 
-for (let job of jobs) {
-    let count = 0;
-    for (let i = 0; i < job; i++) {
-        count++;
+function chunkify(array, n) {
+    let chunks = [];
+    for (let i = n; i > 0; i--) {
+        chunks.push(array.splice(0, Math.ceil(array.length / i)));
     }
+    return chunks;
 }
 
-const tock = performance.now();
+function run(jobs, concurrentWorkers) {
+    const chunks = chunkify(jobs, concurrentWorkers);
 
-console.log(`Main thread took ${tock - tick} ms`);
+    const tick = performance.now();
+    let completedWorkers = 0;
+
+    chunks.forEach((data, i) => {
+        const worker = new Worker('./worker.js');
+        worker.postMessage(data);
+
+        worker.on('message', () => {
+            console.log(`Worker ${i} completed`);
+
+            completedWorkers++;
+            if (completedWorkers === concurrentWorkers) {
+                console.log(
+                    `${concurrentWorkers} Workers took ${performance.now()-tick}`
+                );
+                process.exit();
+            }
+        });
+    });
+}
+
+run(jobs, 16);
